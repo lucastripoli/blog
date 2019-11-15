f625d0ea0e6dee8df890f73d31d8d68e62633f12
lbeltrao <lucas.beltrao@itelios.com>
2019-11-15 17:03:45 -0300

    First version of a Gatsby blog static site

---
+++ component---src-pages-404-js-89f9e5c8e9898d216b74.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{264:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-89f9e5c8e9898d216b74.js.map
